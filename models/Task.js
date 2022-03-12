a68b40991c2c3163ea48847c60272da26b55c6ac
Jonathan F-Carait <jonathan.carait@outlook.com>
2022-03-12 17:56:52 +1100

    created task model

---
+++ models/Task.js
@@ -0,0 +1,48 @@
+// import important parts of sequelize library
+const { Model, DataTypes } = require('sequelize');
+// import our databse connection from config.js
+const sequelize = require('../config/connection');
+
+// Initialise Task model (table) by extending off Sequelize's Model class
+class Task extends Model { }
+
+// set up fields and rules for Task model
+Task.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        task_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        date: {
+            type: DataTypes.DATE,
+            allowNull: true,
+        },
+        duration: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        location_id {
+    type: DataTypes.INTEGER,
+    references: {
+        model: 'location',
+        key: 'id',
+    },
+},
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'task',
+    }
+);
