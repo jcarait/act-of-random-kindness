fd18e4744387d2cc6e73b009082dcf129d05b1e5
Jonathan F-Carait <jonathan.carait@outlook.com>
2022-03-14 20:08:01 +1100

    setup index routes

---
+++ controllers/api/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const taskRoutes = require('./task-routes');
+const userRoutes = require('./user-routes');
+const taskLocationRoutes = require('./task-location-routes');
+
+router.use('/tasks', taskRoutes);
+router.use('/users', userRoutes);
+router.use('/task-locations', taskLocationRoutes);
+
+module.exports = router;
