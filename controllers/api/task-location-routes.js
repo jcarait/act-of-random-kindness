2fc68707eaafbc1d484cf55419077ec436a2d1a5
Roman Ac <roman25xx@gmail.com>
2022-03-15 19:54:18 +1100

    WIP

--- controllers/api/task-location-routes.js
+++ controllers/api/task-location-routes.js
@@ -9,6 +9,19 @@ router.get('/', async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
-})
+});
+
+// router.post('/', withAuth, async (req, res) => {
+//   try {
+//     const newTask = await Task.create({
+//       ...req.body,
+//       user_id: req.session.user_id,
+//     });
+
+//     res.status(200).json(newTask);
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
 
 module.exports = router;
